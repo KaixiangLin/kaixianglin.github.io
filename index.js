9f60dc3990eff06233c9de5796ab3d22c16fc9c0
Kaixiang Lin <linkaixi@msu.edu>
2020-12-18 18:44:26 -0500

    Update website template

---
+++ index.js
@@ -0,0 +1,35 @@
+function visibility_on(id) {
+    var e = document.getElementById(id+"_text");
+    if(e.style.display == 'none')
+        e.style.display = 'block';
+    var e = document.getElementById(id+"_img");
+    if(e.style.display == 'none')
+        e.style.display = 'block';
+}
+function visibility_off(id) {
+    var e = document.getElementById(id+"_text");
+    if(e.style.display == 'block')
+        e.style.display = 'none';
+    var e = document.getElementById(id+"_img");
+    if(e.style.display == 'block')
+        e.style.display = 'none';
+}
+function toggle_visibility(id) {
+    var e = document.getElementById(id+"_text");
+    if(e.style.display == 'inline')
+        e.style.display = 'block';
+    else
+        e.style.display = 'inline';
+    var e = document.getElementById(id+"_img");
+    if(e.style.display == 'inline')
+        e.style.display = 'block';
+    else
+        e.style.display = 'inline';
+}
+function toggle_vis(id) {
+    var e = document.getElementById(id);
+    if (e.style.display == 'none')
+        e.style.display = 'inline';
+    else
+        e.style.display = 'none';
+}
--- javascripts/main.js
+++
@@ -1,1 +0,0 @@
-// alert('hi this is a test');
